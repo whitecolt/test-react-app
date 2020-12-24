cd10a8647c3c18a89336bd5a6d555762dabef398
whitecolt <whitecolt8@gmail.com>
2020-12-24 18:39:47 +0300

    Updates

---
+++ precache-manifest.98279ee6f83f89efe4c761baf68e2fa9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32b6228032250028eda6be0a556be941",
+    "url": "/whitecolt.github.io/test-react-app/index.html"
+  },
+  {
+    "revision": "3c23f0a55d2529042877",
+    "url": "/whitecolt.github.io/test-react-app/static/css/main.5cb4d569.chunk.css"
+  },
+  {
+    "revision": "972e446d2a5e2d4d67eb",
+    "url": "/whitecolt.github.io/test-react-app/static/js/2.f330f109.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/whitecolt.github.io/test-react-app/static/js/2.f330f109.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c23f0a55d2529042877",
+    "url": "/whitecolt.github.io/test-react-app/static/js/main.cd371fd1.chunk.js"
+  },
+  {
+    "revision": "56b2e3f5b9c85c330297",
+    "url": "/whitecolt.github.io/test-react-app/static/js/runtime-main.25657b2b.js"
+  }
+]);
